83f806d0dd8dd2d70c96a73c2197f95d58d97bad
adithya-naik <jatothadithyanaik@gmail.com>
2026-06-14 22:08:02 +0530

    add : header and footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import {ListEmployeeComponent} from './list-employee/list-employee.component';
-
+import {HeaderComponent} from './header/header.component'
+import {FooterComponent} from './footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive,ListEmployeeComponent],
+  imports: [RouterOutlet,HeaderComponent,ListEmployeeComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
