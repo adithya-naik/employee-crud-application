29726a5750a0b72fa659b925db0baccc15df4000
adithya-naik <jatothadithyanaik@gmail.com>
2026-06-14 21:59:00 +0530

    add : angular routing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import {ListEmployeeComponent} from './list-employee/list-employee.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ListEmployeeComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive,ListEmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { ListEmployeeComponent } from './list-employee/list-employee.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'employees',
+    component: ListEmployeeComponent
+  },
+
+  {
+    path: 'create-employee',
+    component: CreateEmployeeComponent
+  },
+
+  {
+    path: '',
+    redirectTo: 'employees',
+    pathMatch: 'full'
+  }
+
+];
