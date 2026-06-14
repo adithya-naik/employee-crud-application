18a5291473812cb3bd69a7b1e0889f037c05d381
adithya-naik <jatothadithyanaik@gmail.com>
2026-06-14 19:28:30 +0530

    create employee list ui with angular components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ListEmployeeComponent} from './list-employee/list-employee.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ListEmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
