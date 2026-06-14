fe144d1bdaaf7ba905d5834804dd964405680df2
adithya-naik <jatothadithyanaik@gmail.com>
2026-06-14 23:06:12 +0530

    add : search functionality

--- frontend/src/app/list-employee/list-employee.component.ts
+++ frontend/src/app/list-employee/list-employee.component.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {EmployeeService} from '../employee.service';
 import {Employee} from '../employee';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-list-employee',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './list-employee.component.html',
   styleUrl: './list-employee.component.css'
 })
 export class ListEmployeeComponent {
 
   employees : Employee[] = []
-
+  filteredEmployees: Employee[] = [];
+  searchText: string = '';
   constructor(private employeeService:EmployeeService) { }
 
   ngOnInit():void{
@@ -21,8 +22,21 @@ export class ListEmployeeComponent {
 
   private getEmployee(){
     this.employeeService.getEmployeesList().subscribe(data=>{
-      this.employees=data;
+      this.employees = data;
+      this.filteredEmployees = data;
     })
+  }
+
+  searchEmployee() {
+
+    this.filteredEmployees = this.employees.filter(employee =>
+
+      employee.firstName.toLowerCase().includes(this.searchText.toLowerCase()) ||
+
+      employee.lastName.toLowerCase().includes(this.searchText.toLowerCase())
+
+    );
+
   }
 
 }
